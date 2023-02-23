2ebc2f70bf50dd9edd68036f4ab6f78cf7931297
LiubovDav <lubasha0512@gmail.com>
2023-02-22 19:50:54 -0800

    Added Spring Boot

---
+++ src/main/java/org/liubov/statetaxcalculator/StateTaxCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.liubov.statetaxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateTaxCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateTaxCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/liubov/statetaxcalculator/StateTaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liubov.statetaxcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateTaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
