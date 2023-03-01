a5b68492379655ee303c62c71d5ac8b621d11a8e
LiubovDav <lubasha0512@gmail.com>
2023-03-01 14:38:02 -0800

    Added Home and Login pages. Added links between pages. Renamed pages.

--- src/main/java/org/liubov/statetaxcalculator/controller/WebController.java
+++ src/main/java/org/liubov/statetaxcalculator/controller/WebController.java
@@ -14,20 +14,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class WebController {
 
-    @GetMapping("/register_form")
-    public String registerForm(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "register_form";
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        return "home";
     }
 
-    @PostMapping("/register_user")
-    public String registerUser(@ModelAttribute("user") User user) {
+    @GetMapping("/sign_up")
+    public String signUp(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "sign_up";
+    }
+
+    @PostMapping("/sign_up_submit")
+    public String signUpSubmit(@ModelAttribute("user") User user) {
         log.info("User = " + user);
 
         // todo: save the User to the DB
 
-        return "register_success";
+        return "sign_up_success";
     }
 
+    @GetMapping("/login")
+    public String login(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "login";
+    }
+
+    @PostMapping("/login_submit")
+    public String loginSubmit(@ModelAttribute("user") User user) {
+        log.info("User = " + user);
+
+        // todo: validate user email and password
+
+        return "login_success";
+    }
 }
